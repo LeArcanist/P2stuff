4e7e6f2624ee07345a17f6355e65114541b91d70
Zheng Yang <s10178386@connect.np.edu.sg>
2019-01-02 11:54:12 +0800

    basic code for fragments

---
+++ app/src/main/java/com/example/zhengyang/tabstesting/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.zhengyang.tabstesting;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private ViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.pager);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/example/zhengyang/tabstesting/ViewPagerAdapter.java
@@ -0,0 +1,29 @@
+package com.example.zhengyang.tabstesting;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        DemoFragment demoFragment = new DemoFragment();
+        position = position + 1;
+        Bundle bundle = new Bundle();
+        bundle.putString("message","Fragment: " + position);
+        demoFragment.setArguments(bundle);
+
+        return demoFragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
